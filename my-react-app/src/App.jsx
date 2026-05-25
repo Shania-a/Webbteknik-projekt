593f63f55b7395eab5c3e6805e8c1d3f869e3eae
GHL <gustavhl@hotmail.com>
2026-05-25 16:02:55 +0200

    Added basic date call

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -6,13 +6,18 @@ import Test from './components/Test/Test.jsx'
 
 async function getAPIData(){
   const api_key = import.meta.env.VITE_NASA_API_KEY; 
+  const date = "";
+  const start = "2000-01-01";
+  const end = "2000-01-07";
 
-  const data = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${api_key}`);
-  const jsonData = await data.json()
 
-  console.log("NASA Data:", jsonData);
-  console.log("Title:", jsonData.title);
-  console.log("Image URL:", jsonData.url);
+  const url = date
+    ? await fetch(`https://api.nasa.gov/planetary/apod?api_key=${api_key}&date=${date}`)
+    : await fetch(`https://api.nasa.gov/planetary/apod?api_key=${api_key}`);
+  const jsonData = await url.json()
+
+  console.log("Data:", jsonData);
+  console.log("URL:", jsonData.url);
 
   return jsonData;
 };
@@ -46,7 +51,7 @@ function App() {
           <img src={nasaData.url} />
         </div>
       ) : (
-        <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.freepik.com%2Fpremium-vector%2Floading-symbol-vector_427757-728.jpg%3Fw%3D2000&f=1&nofb=1&ipt=081322b4315587828aa42c0487603103677eb4efe9993342aa78e1a8a9826472' />
+        <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.freepik.com%2Fpremium-vector%2Floading-symbol-vector_427757-728.jpg%3Fw%3D2000&f=1&nofb=1&ipt=081322b4315587828aa42c0487603103677eb4efe9993342aa78e1a8a9826472' style={{maxWidth: '100%'}}/>
       )}
     </>
   )
