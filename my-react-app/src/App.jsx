1bd97f19a179c42c1373b74720b3989a1c97645e
swedishllama1 <204344850+swedishllama1@users.noreply.github.com>
2026-05-27 16:51:42 +0200

    Experiment med vite, tailwind, sen bootstrap, sen gjort navbar komponent

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import Test from './components/Test/Test.jsx'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import NavbarTest from './components/Test/NavbarTest/NavbarTest.jsx';
 
 
 
 async function getAPIData(){
   const api_key = import.meta.env.VITE_NASA_API_KEY; 
-  const date = "2026-01-01";
+  const date = "";
   // const start = "2000-01-01";
   // const end = "2000-01-07";
 
@@ -44,17 +49,30 @@ function App() {
   }, []);
 
   return (
-    <>
-    <Test/>
-    {nasaData ? (
-        <div>
-          <img src={nasaData.url} />
-        </div>
-      ) : (
-        <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.freepik.com%2Fpremium-vector%2Floading-symbol-vector_427757-728.jpg%3Fw%3D2000&f=1&nofb=1&ipt=081322b4315587828aa42c0487603103677eb4efe9993342aa78e1a8a9826472' style={{maxWidth: '100%'}}/>
-      )}
-    </>
-  )
+    <Container>
+      <Row>
+        <Col><h1>Spacefullness</h1></Col>
+      </Row>
+      <Row>
+        <Col>
+            {nasaData ? (
+            <div>
+              <img src={nasaData.url} />
+            </div>
+            ) : (
+            <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.freepik.com%2Fpremium-vector%2Floading-symbol-vector_427757-728.jpg%3Fw%3D2000&f=1&nofb=1&ipt=081322b4315587828aa42c0487603103677eb4efe9993342aa78e1a8a9826472' style={{maxWidth: '100%'}}/>
+            )}
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+            <NavbarTest />
+        </Col>
+      </Row>
+    </Container>
+    
+    
+  );
 }
 
 export default App
