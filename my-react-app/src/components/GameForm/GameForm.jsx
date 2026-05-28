c76c1dca108ee0a0386fb1783c02ae781b696c5b
swedishllama1 <204344850+swedishllama1@users.noreply.github.com>
2026-05-28 22:15:57 +0200

    Added some game funktionalities, GameForm component, able to save to localstorage

---
+++ my-react-app/src/components/GameForm/GameForm.jsx
@@ -0,0 +1,46 @@
+import Form from 'react-bootstrap/Form';
+import Buttons from '../Buttons/Buttons.jsx';
+import { useState } from 'react';
+
+function GameForm ({ onStartGame }) {
+
+    const [username, setUsername] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (username.trim().length > 0) {
+           localStorage.setItem("game-username", username); 
+
+           onStartGame();
+        } else {
+            alert("Please enter a username to play!");
+        }
+    };
+
+    return (
+        <div className="form-overlay">
+            <Form onSubmit={handleSubmit} className="bg-dark p-4 rounded text-light shadow-lg">
+                <h4 className="mb-3 text-center">Who's playing?</h4>
+
+                <Form.Group className="mb-3">
+                    <Form.Label>Username:</Form.Label>
+                    <Form.Control 
+                    type="text" 
+                    placeholder="Enter username here"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)} 
+                    />
+                    <Form.Text className="text-muted">
+                    Your username will only be saved to your Localstorage!
+                    </Form.Text>
+                </Form.Group>
+
+                <div className="text-center">
+                    <Buttons type="submit" text="Save and Start!" />
+                </div>
+            </Form>
+        </div>
+    );
+}
+
+export default GameForm;
