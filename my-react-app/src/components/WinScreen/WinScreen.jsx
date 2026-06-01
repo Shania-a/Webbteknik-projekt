73baa5fa2d2fc1dd4ab4b8863836751a594feae5
GHL <gustavhl@hotmail.com>
2026-06-01 19:07:52 +0200

    Added Winscreen modal
    Istalled react confetti.
    Added react boottrap modal for win screen.

---
+++ my-react-app/src/components/WinScreen/WinScreen.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import Confetti from 'react-confetti';
+
+const WinSceen = ({ show, handleClose, date }) => {
+
+    const navigate = useNavigate();
+
+    const handleGoToArchive = () => {
+        handleClose();
+        navigate('/archive');
+    };
+
+    return (
+        <>
+        {show && (
+                <Confetti
+                    numberOfPieces={200}
+                    recycle={true} 
+                    style={{
+                        position: 'fixed',
+                        top: 0,
+                        left: 0,
+                        width: '100vw',
+                        height: '100vh',
+                        zIndex: 2000 
+                    }}
+                />
+            )}
+        <Modal
+            show={show}
+            onHide={handleClose}
+            centered
+            backdrop="static"
+            keyboard={false}
+            
+        >
+        
+            <Modal.Header className="bg-primary bg-dark text-light">
+                <Modal.Title>Win</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body className="text-center py-4 bg-dark text-light">
+                <p className="lead">Wow you finished:</p>
+                <p className="lead"><strong>{date}</strong>.</p>
+            </Modal.Body>
+            <Modal.Footer className="justify-content-center bg-dark text-light">
+                <Button variant="outline-light" onClick={handleClose}>
+                    Stay Here
+                </Button>
+                <Button variant="primary" onClick={handleGoToArchive}>
+                    Go to Archive
+                </Button>
+            </Modal.Footer>
+            
+        </Modal>
+        </>
+    );
+};
+
+export default WinSceen;
