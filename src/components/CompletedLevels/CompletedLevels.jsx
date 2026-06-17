e6511c0cbd3fb9ffc4184da0936eede87ccb4599
swedishllama1 <204344850+swedishllama1@users.noreply.github.com>
2026-06-17 22:27:47 +0200

    Added styling on the user-page

--- src/components/CompletedLevels/CompletedLevels.jsx
+++ src/components/CompletedLevels/CompletedLevels.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
+import './CompletedLevels.css';
 
 const CompletedLevels = ({ completedDates }) => {
     if (!completedDates || completedDates.length === 0) {
         return <p>No completed levels yet! Go out and explore the galaxy!</p>
     }
     return (
-        <ListGroup>
+        <ListGroup className="completed-list">
             {completedDates.map((game, index) => (
                 <ListGroup.Item
                     key={index}
-                    className="d-flex align-items-center mb-2 bg-dark text-light border-secondary rounded"
+                    className="completed-item d-flex flex-column align-items-center mb-2 bg-dark text-light border-secondary rounded"
                 
                 >
-                <div>
+                <div className="completed-info">
                     <h5 className="mb-0">{game.title}</h5>
                     <p className="small">{game.date}</p>
                     {/*If there is a stopwatch-time, include it */}
                     <p className="small">{game.timeTaken}</p>
                 </div>
                 <img
+                    className="completed-img"
                     src={game.imageUrl}
                     alt={game.title}
-                    style={{ width: '60px', height: '60px', objectFit: 'cover', borderRadius: '5px'}}></img>
+                    />
                 </ListGroup.Item>
             ))}
         </ListGroup>
